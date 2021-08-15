a1545a64000d0cace12a84ed5b4a6595dc125b89
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-16 05:21:43 +0530

    feat: area

--- src/area.js
+++ src/area.js
@@ -1,33 +1,30 @@
-var areaCalBlock = document.querySelectorAll(".area-block");
-var options = document.querySelectorAll(".radio");
-var result = document.querySelectorAll(".output");
+var sides = document.querySelectorAll(".side");
+var result = document.querySelector(".output");
 
-var inputInOption1 = document.querySelectorAll(".inputInOption1");
-var inputInOption2 = document.querySelectorAll(".inputInOption2");
-var inputInOption3 = document.querySelectorAll(".inputInOption3");
+var base, height;
 
-var errorDiv = document.querySelector(".error");
-var optionOne = document.getElementById("Option1");
-var optionTwo = document.getElementById("Option2");
-var optionThree = document.getElementById("Option3");
-var calculate = document.querySelectorAll(".submit");
-
-document.addEventListener("continue", clickHandler);
+function multiplyBaseAndHeight(b, h) {
+  const multipliedOutput = b * h;
+  return multipliedOutput;
+}
 
 function clickHandler(e) {
   e.preventDefault();
-  optionThree.classList.add("hidden");
-  optionOne.classList.add("hidden");
-  optionTwo.classList.add("hidden");
-
-  if (options[0].checked) {
-    optionOne.classList.remove("hidden");
-    calculate[0].addEventListener("click", optionOneCalculate);
-  } else if (options[1].checked) {
-    optionTwo.classList.remove("hidden");
-    calculate[1].addEventListener("click", optionTwoCalculate);
-  } else if (options[2].checked) {
-    optionThree.classList.remove("hidden");
-    calculate[2].addEventListener("click", optionThreeCalculate);
-  }
+  hide();
+  base = Number(sides[0].value);
+  height = Number(sides[1].value);
+  const basexheight = multiplyBaseAndHeight(base, height);
+  const area = basexheight / 2;
+  show("Area of Triangle: ", area);
 }
+
+function show(text1, text2) {
+  result.style.display = "block";
+  result.innerText = text1 + text2;
+}
+
+function hide() {
+  result.style.display = "none";
+}
+
+document.addEventListener("submit", clickHandler);
