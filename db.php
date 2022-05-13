d7e1fb890feea7bde543d5854c95782b0b8d3a04
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2022-05-13 22:20:56 +0530

    added php for college

--- db.php
+++ db.php
@@ -1,14 +1,28 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
+$databasename = "triangle_login";
+
+$v1 = $_POST["email"];
+$v2 = $_POST["password"];
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $databasename);
 // Check connection
-if ($conn->connect_error) {
+if ($conn->connect_error) 
+{
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$result = mysqli_query($conn, "INSERT INTO `login` (`E_mail`,`password`) VALUES ('$v1','$v2')");
+
+if ($result) 
+{
+  echo "Record added successfully!";
+}
+else 
+{
+  echo "Couldn't add record. Reason: " . mysqli_error($conn) . "<br>";
+}
 ?>
